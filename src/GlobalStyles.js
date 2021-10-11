037fff9196c16a9f7ded5ffdb3481dc7d057a72e
Falk Kreimeier <f.kreimeier@gmx.net>
2021-10-11 15:21:10 +0200

    add GlobalStyles

---
+++ src/GlobalStyles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components'
+import Wisdom from './Font/Wisdom.otf'
+
+const GlobalStlyles = createGlobalStyle`
+@font-face {
+    font-family: wisdom;
+    src: url(${Wisdom}) format("openType");
+}
+
+html {
+    box-sizing: border-box;
+}
+
+* {
+    box-sizing: inherit;
+}
+
+body {
+    background-color: black;
+}
+
+`
+export default GlobalStlyles
