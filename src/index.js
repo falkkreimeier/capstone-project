66f331684e1299bf2eb53ee37feb7f3a880b8f7f
Falk Kreimeier <f.kreimeier@gmx.net>
2021-10-11 15:20:12 +0200

    build data.json, map it and add in ProductCard

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import data from './data.json'
+import GlobalStlyles from '../src/GlobalStyles.js'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalStlyles />
+    <App data={data} />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
