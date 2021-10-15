42843dc8dc838a3e3cda739fb1df93aa71a84e58
Falk Kreimeier <f.kreimeier@gmx.net>
2021-10-15 18:28:03 +0200

    add memoryRouter to Storybook

---
+++ src/components/Footer.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from './Footer'
+
+export default {
+  title: 'component/Footer',
+  component: Footer,
+}
+
+const Template = args => <Footer {...args} />
+
+export const Standard = Template.bind({})
+Standard.args = {}
