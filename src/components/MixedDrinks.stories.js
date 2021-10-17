17d8e22e7d611e3b290dddd8c27655d67324d6af
Falk Kreimeier <f.kreimeier@gmx.net>
2021-10-17 19:59:50 +0200

    add mixedDrinks to Storybook

---
+++ src/components/MixedDrinks.stories.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import MixedDrinks from './MixedDrinks'
+
+export default {
+  title: 'component/MixedDrinks',
+  component: MixedDrinks,
+}
+
+const Template = args => <MixedDrinks {...args} />
+
+export const Standard = Template.bind({})
+Standard.args = {
+  mixedDrinks: 'Shot',
+  name: 'Wahnsinniger Bayleys',
+  ingredience: 'Lakritzel und Bayleys',
+  preparation: 'Bayleys und Lakritzel zu gleichen Teilen in ein Pinnchen',
+}
