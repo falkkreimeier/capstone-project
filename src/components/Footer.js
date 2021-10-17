4f7e45efecef881379f98468ac88c82ead1ecdbc
Falk Kreimeier <f.kreimeier@gmx.net>
2021-10-17 17:46:09 +0200

    add Router and Link to mixedDrinks in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,32 +10,45 @@ function Footer() {
       <Link to="/motive">
         <Motiv src="./img/Logo.jpg" alt="Motive" />
       </Link>
+      <Link to="/mixedDrinks">
+        <Cocktails src="./img/Pinnchen.jpg" alt="Cocktails and Shots" />
+      </Link>
+      <DoodleHead src="./img/Portfolio.jpg" alt="The Man behind the Monster" />
     </Wrapper>
   )
 }
 
 const Wrapper = styled.footer`
   display: grid;
-  grid-template-areas: 'product campaign cocktails';
-  justify-content: flex-start;
+  grid-template-areas: 'product campaign cocktails doodleHead';
   align-items: center;
   margin: 0 auto;
-  margin-bottom: 10px;
   border-radius: 19px;
   background-color: white;
-  width: 22rem;
+  width: var(--main-width);
   height: 5rem;
 `
 
 const Product = styled.img`
-  margin-left: 15px;
+  margin-left: 10px;
   grid-area: product;
   height: 4rem;
 `
 
 const Motiv = styled.img`
-  margin-left: 15px;
+  margin-left: 10px;
   grid-area: campaign;
+  height: 4rem;
+`
+
+const Cocktails = styled.img`
+  margin-left: 5px;
+  grid-area: cocktails;
+  height: 3.5rem;
+`
+
+const DoodleHead = styled.img`
+  grid-area: doodleHead;
   height: 4rem;
 `
 
