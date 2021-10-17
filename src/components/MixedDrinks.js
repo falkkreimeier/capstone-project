f066153d35ebc0f6daa6a62f06d072eefa76cf32
Falk Kreimeier <f.kreimeier@gmx.net>
2021-10-17 17:47:11 +0200

    add new Componenten

---
+++ src/components/MixedDrinks.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+function MixedDrinks({ name, mixed, ingredience, preparation }) {
+  return (
+    <Wrapper>
+      <Headline>
+        {mixed}: {name}
+      </Headline>
+      <Ingredience>Zutaten: {ingredience}</Ingredience>
+      <MixIt>Zuberereitung: {preparation}</MixIt>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  border-radius: var(--border-radius);
+  margin: 0 auto;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  /* justify-content: center;
+  align-items: center; */
+  width: var(--main-width);
+`
+
+const Headline = styled.h2`
+  margin: 0 auto;
+  font: 'Ventana';
+  margin-bottom: 5px;
+`
+
+const Ingredience = styled.div`
+  margin-bottom: 5px;
+`
+
+const MixIt = styled.div`
+  margin-bottom: 5px;
+`
+
+export default MixedDrinks
