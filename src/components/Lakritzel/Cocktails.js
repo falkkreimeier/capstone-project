a9ff6800614279d7994a82045a16760cac03f591
Falk Kreimeier <f.kreimeier@gmx.net>
2021-10-22 14:58:37 +0200

    delete unnecessary code

--- src/components/Lakritzel/Cocktails.js
+++ src/components/Lakritzel/Cocktails.js
@@ -1,11 +1,9 @@
 import styled from 'styled-components/macro'
 
-function Cocktails({ name, mixed, ingredients, preparation }) {
+function Cocktails({ name, ingredients, preparation }) {
   return (
     <Wrapper>
-      <Headline>
-        {mixed} {name}
-      </Headline>
+      <Headline>{name}</Headline>
       <InnerHeadline>Zutaten:</InnerHeadline>
       <Ingredience>{ingredients}</Ingredience>
       <InnerHeadline>Zubereitung:</InnerHeadline>
@@ -22,31 +20,40 @@ const Wrapper = styled.section`
   flex-direction: column;
   padding: 10px;
   width: var(--main-width);
+  min-height: 20rem;
   gap: 5px;
-  height: auto;
+  :nth-child(even) {
+    background-color: black;
+    border: 20px solid white;
+    color: white;
+  }
+  h4:nth-child(3n, 4n) {
+    color: white;
+  }
 `
 
 const Headline = styled.h2`
-  margin: 0 auto;
   font-family: 'Ventana';
   margin-bottom: 10px;
+  margin: 20px auto;
 `
 
 const InnerHeadline = styled.h4`
   font-family: var(--main-headline);
   color: #58555a;
-  margin: 0;
   margin-bottom: 5px;
+  margin: 10px 0 0 30px;
 `
 
 const Ingredience = styled.div`
   font-family: var(--main-font);
   margin-bottom: 10px;
+  margin: 0 0 0 30px;
 `
 
 const MixIt = styled.div`
   font-family: var(--main-font);
   margin-bottom: 5px;
+  margin: 0 0 0 30px;
 `
-
 export default Cocktails
