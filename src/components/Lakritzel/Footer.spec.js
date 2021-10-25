6658d43f474023348e812496434c06f61651d570
Falk Kreimeier <f.kreimeier@gmx.net>
2021-10-25 12:11:23 +0200

    repair the tests

--- src/components/Lakritzel/Footer.spec.js
+++ src/components/Lakritzel/Footer.spec.js
@@ -3,31 +3,14 @@ import Footer from './Footer'
 import { MemoryRouter } from 'react-router'
 
 describe('Footer', () => {
-  it('shows Images', () => {
+  it('shows 4 navLinks', () => {
     render(
       <MemoryRouter>
         <Footer />
       </MemoryRouter>
     )
 
-    const image = screen.getAllByRole('img')
-    expect(image).toHaveLength(4)
-  })
-
-  it('has the correct alt text', () => {
-    const altText = 'Products'
-    const altText2 = 'Motive'
-
-    render(
-      <MemoryRouter>
-        <Footer />
-      </MemoryRouter>
-    )
-
-    const text = screen.getByAltText(altText)
-    expect(text).toHaveAttribute('alt', altText)
-
-    const text2 = screen.getByAltText(altText2)
-    expect(text2).toHaveAttribute('alt', altText2)
+    const nav = screen.getAllByRole('link')
+    expect(nav).toHaveLength(4)
   })
 })
