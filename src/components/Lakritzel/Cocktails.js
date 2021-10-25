4dfcc8702cbe71e5af014e2ea60a18da054661d6
Falk Kreimeier <f.kreimeier@gmx.net>
2021-10-25 12:08:25 +0200

    rename and redesign cocktails

--- src/components/Lakritzel/Cocktails.js
+++ src/components/Lakritzel/Cocktails.js
@@ -1,56 +1,58 @@
 import styled from 'styled-components/macro'
+import klecks from './Assets/Kleckse_clean.jpg'
 
 function Cocktails({ name, ingredients, preparation }) {
   return (
     <Wrapper>
       <Headline>{name}</Headline>
-      <InnerHeadline>Zutaten:</InnerHeadline>
-      <Ingredience>{ingredients}</Ingredience>
-      <InnerHeadline>Zubereitung:</InnerHeadline>
-      <MixIt>{preparation}</MixIt>
+      <Subline>Zutaten:</Subline>
+      <Text>{ingredients}</Text>
+      <Subline>Zubereitung:</Subline>
+      <Text>{preparation}</Text>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
+  background-image: url(${klecks});
+  background-position: center;
+  background-size: 21rem;
+  background-repeat: no-repeat;
+  object-fit: cover;
   border-radius: var(--border-radius);
   margin: 0 auto;
   background-color: white;
   display: flex;
   flex-direction: column;
+  color: white;
   padding: 10px;
   width: var(--main-width);
-  min-height: 20rem;
+  min-height: 22rem;
   gap: 5px;
-  :nth-child(even) {
-    background-color: black;
-    border: 20px solid white;
-    color: white;
+  &:nth-child(even) {
+    background-image: none;
+    color: black;
   }
 `
 
 const Headline = styled.h2`
+  height: 40px;
   font-family: 'Ventana';
-  margin-bottom: 10px;
-  margin: 20px auto;
+  margin: 40px auto 0px auto;
+  z-index: 20;
 `
 
-const InnerHeadline = styled.h4`
+const Subline = styled.h4`
   font-family: var(--main-headline);
-  color: #58555a;
   margin-bottom: 5px;
-  margin: 10px 0 0 30px;
+  margin: 0px 0 0 30px;
+  z-index: 20;
 `
 
-const Ingredience = styled.div`
+const Text = styled.p`
   font-family: var(--main-font);
-  margin-bottom: 10px;
-  margin: 0 0 0 30px;
-`
-
-const MixIt = styled.div`
-  font-family: var(--main-font);
-  margin-bottom: 5px;
-  margin: 0 0 0 30px;
+  padding-right: 20px;
+  margin: 0px 0 10px 30px;
+  z-index: 20;
 `
 export default Cocktails
