64009b56de761b00956b4f253c373b5d67d65f09
Falk Kreimeier <f.kreimeier@gmx.net>
2021-10-27 17:21:57 +0200

    delete unnecessary code

--- src/components/People/Welcome.js
+++ src/components/People/Welcome.js
@@ -27,9 +27,6 @@ const Wrapper = styled.div`
   background-repeat: no-repeat;
   background-size: 21rem;
   background-color: white;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   color: white;
   border-radius: var(--border-radius);
   padding: var(--main-padding);
