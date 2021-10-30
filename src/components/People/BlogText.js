b1ddf1482272a859a007d58a689377b4b54df813
Falk Kreimeier <f.kreimeier@gmx.net>
2021-10-30 17:31:31 +0200

    style blog Text Link

--- src/components/People/BlogText.js
+++ src/components/People/BlogText.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components/macro'
-import klecks from '../Lakritzel/Assets/Kleckse_clean.jpg'
 
 function BlogText() {
   return (
@@ -11,18 +10,17 @@ function BlogText() {
         Es gibt Hüftsteak, Filetsteak, Rumpsteak, T-Bone Steak, Rib-Eye Steak,
         Tomahawk Steak...
       </Text>
-      <LinkToBlog href="https://www.maredo.com/flanksteak-interview-mit-dem-maredo-fleischexperten-martin-ostermeier/">
-        weiterlesen
-      </LinkToBlog>
+      <LinkContainer>
+        <LinkToBlog href="https://www.maredo.com/flanksteak-interview-mit-dem-maredo-fleischexperten-martin-ostermeier/">
+          weiterlesen
+        </LinkToBlog>
+      </LinkContainer>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
   min-height: 352px;
-  background-image: url(${klecks});
-  background-position: center;
-  background-repeat: no-repeat;
   background-size: 21rem;
   background-color: white;
   color: white;
@@ -30,29 +28,34 @@ const Wrapper = styled.div`
   padding: var(--main-padding);
 `
 
-const LinkToBlog = styled.a`
-  text-align: center;
-  margin: 0 auto;
+const LinkContainer = styled.div`
+  margin: 5px auto;
   width: 130px;
-  height: auto;
   text-decoration: none;
   border: 1px solid black;
   padding: var(--main-padding);
-  border-radius: 40px 40px 40px 40px;
-  background-color: white;
+  border-radius: 40px;
+  background-color: black;
   font-family: var(--main-font);
-  color: black;
+  box-shadow: 0 14px 8px rgba(0, 0, 0, 0.25);
   transform: translateY(-4px);
   &:active {
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.8);
     transform: translateY(-0px);
     transition: 0.1s;
   }
+`
+
+const LinkToBlog = styled.a`
+  color: white;
+  text-decoration: none;
 `
 
 const Text = styled.p`
   padding: 15px;
   margin-top: -40px;
   font-family: var(--main-font);
+  color: black;
 `
 
 const Headline = styled.h2`
@@ -61,7 +64,7 @@ const Headline = styled.h2`
   text-align: center;
   font-size: 1.2rem;
   margin-top: 50px;
-  color: white;
+  color: black;
 `
 
 export default BlogText
