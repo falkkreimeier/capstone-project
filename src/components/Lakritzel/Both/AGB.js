1d99262f4afc9135c582c83eba82af743c65c4c6
Falk Kreimeier <f.kreimeier@gmx.net>
2021-11-11 12:01:18 +0100

    add AGBs

---
+++ src/components/Lakritzel/Both/AGB.js
@@ -0,0 +1,269 @@
+import styled from 'styled-components/macro'
+
+function AGB() {
+  return (
+    <Wrapper>
+      <Text>
+        Allgemeine Geschäftsbedingungen der Firma Lakritzel
+        <br /> <br />
+        §1 Geltung gegenüber Unternehmern und Begriffsdefinitionen (1) Die
+        nachfolgenden Allgemeinen Geschäftbedingungen gelten für alle
+        Lieferungen zwischen uns und einem Verbraucher in ihrer zum Zeitpunkt
+        der Bestellung gültigen Fassung.
+        <br /> <br />
+        Verbraucher ist jede natürliche Person, die ein Rechtsgeschäft zu
+        Zwecken abschließt, die überwiegend weder ihrer gewerblichen noch ihrer
+        selbständigen beruflichen Tätigkeit zugerechnet werden können (§ 13
+        BGB).
+        <br /> <br />
+        §2 Zustandekommen eines Vertrages, Speicherung des Vertragstextes (1)
+        Die folgenden Regelungen über den Vertragsabschluss gelten für
+        Bestellungen über unseren Internetshop http://www.lakritzel.com .
+        <br /> <br />
+        (2) Im Falle des Vertragsschlusses kommt der Vertrag mit
+        <br /> <br />
+        Lakritzel
+        <br /> <br />
+        Falk Kreimeier
+        <br /> <br />
+        Gesundheitstr. 11
+        <br /> <br />
+        D-42699 Solingen
+        <br /> <br />
+        zustande.
+        <br /> <br />
+        (3) Die Präsentation der Waren in unserem Internetshop stellen kein
+        rechtlich bindendes Vertragsangebot unsererseits dar, sondern sind nur
+        eine unverbindliche Aufforderungen an den Verbraucher, Waren zu
+        bestellen. Mit der Bestellung der gewünschten Ware gibt der Verbraucher
+        ein für ihn verbindliches Angebot auf Abschluss eines Kaufvertrages ab.
+        (4) Bei Eingang einer Bestellung in unserem Internetshop gelten folgende
+        Regelungen: Der Verbraucher gibt ein bindendes Vertragsangebot ab, indem
+        er die in unserem Internetshop vorgesehene Bestellprozedur erfolgreich
+        durchläuft.
+        <br /> <br />
+        Die Bestellung erfolgt in folgenden Schritten:
+        <br /> <br />
+        1) Auswahl der gewünschten Ware
+        <br /> <br />
+        2) Bestätigen durch Anklicken der Buttons „Bestellen“
+        <br /> <br />
+        3) Prüfung der Angaben im Warenkorb
+        <br /> <br />
+        4) Betätigung des Buttons „zur Kasse“
+        <br /> <br />
+        5) Anmeldung im Internetshop nach Registrierung und Eingabe der
+        Anmelderangaben (E-Mail-Adresse und Passwort).
+        <br /> <br />
+        6) Nochmalige Prüfung bzw. Berichtigung der jeweiligen eingegebenen
+        Daten.
+        <br /> <br />
+        7) Verbindliche Absendung der Bestellung durch Anklicken des Buttons
+        „kostenpflichtig bestellen“ bzw. „kaufen“
+        <br /> <br />
+        Der Verbraucher kann vor dem verbindlichen Absenden der Bestellung durch
+        Betätigen der in dem von ihm verwendeten Internet-Browser enthaltenen
+        „Zurück“-Taste nach Kontrolle seiner Angaben wieder zu der Internetseite
+        gelangen, auf der die Angaben des Kunden erfasst werden und
+        Eingabefehler berichtigen bzw. durch Schließen des Internetbrowsers den
+        Bestellvorgang abbrechen. Wir bestätigen den Eingang der Bestellung
+        unmittelbar durch eine automatisch generierte E-Mail
+        („Auftragsbestätigung“). Mit dieser nehmen wir Ihr Angebot an.
+        <br /> <br />
+        (5) Speicherung des Vertragstextes bei Bestellungen über unseren
+        Internetshop : Wir senden Ihnen die Bestelldaten und unsere AGB per
+        E-Mail zu. Die AGB können Sie jederzeit auch unter
+        http://www.lakritzel.com/agb.html einsehen. Ihre Bestelldaten sind aus
+        Sicherheitsgründen nicht mehr über das Internet zugänglich.
+        <br /> <br />
+        §3 Preise, Versandkosten, Zahlung, Fälligkeit
+        <br /> <br />
+        (1) Die angegebenen Preise enthalten die gesetzliche Umsatzsteuer und
+        sonstige Preisbestandteile. Hinzu kommen etwaige Versandkosten.
+        <br /> <br />
+        (2) Der Verbraucher hat die Möglichkeit der Zahlung per Vorkasse,
+        Bankeinzug, PayPal .
+        <br /> <br />
+        (3) Hat der Verbraucher die Zahlung per Vorkasse gewählt, so
+        verpflichtet er sich, den Kaufpreis unverzüglich nach Vertragsschluss zu
+        zahlen.
+        <br /> <br />
+        §4 Lieferung
+        <br />
+        (1) Sofern wir dies in der Produktbeschreibung nicht deutlich anders
+        angegeben haben, sind alle von uns angebotenen Artikel sofort
+        versandfertig. Die Lieferung erfolgt hier spätesten innerhalb von 5
+        Werktagen. Dabei beginnt die Frist für die Lieferung im Falle der
+        Zahlung per Vorkasse am Tag nach Zahlungsauftrag an die mit der
+        Überweisung beauftragte Bank und bei allen anderen Zahlungsarten am Tag
+        nach Vertragsschluss zu laufen. Fällt das Fristende auf einen Samstag,
+        Sonntag oder gesetzlichen Feiertag am Lieferort, so endet die Frist am
+        nächsten Werktag.
+        <br /> <br />
+        (2) Die Gefahr des zufälligen Untergangs und der zufälligen
+        Verschlechterung der verkauften Sache geht auch beim Versendungskauf
+        erst mit der Übergabe der Sache an den Käufer auf diesen über.
+        <br /> <br />
+        §5 Eigentumsvorbehalt
+        <br />
+        Wir behalten uns das Eigentum an der Ware bis zur vollständigen
+        Bezahlung des Kaufpreises vor.
+        <br /> <br />
+        ****************************************************************************************************
+        §6 Widerrufsrecht des Kunden als Verbraucher:
+        <br />
+        Widerrufsrecht für Verbraucher
+        <br /> <br />
+        Verbrauchern steht ein Widerrufsrecht nach folgender Maßgabe zu, wobei
+        Verbraucher jede natürliche Person ist, die ein Rechtsgeschäft zu
+        Zwecken abschließt, die überwiegend weder ihrer gewerblichen noch ihrer
+        selbständigen beruflichen Tätigkeit zugerechnet werden können:
+        <br /> <br />
+        Widerrufsbelehrung
+        <br /> <br />
+        Widerrufsrecht
+        <br /> <br />
+        Sie haben das Recht, binnen vierzehn Tagen ohne Angabe von Gründen
+        diesen Vertrag zu widerrufen.
+        <br /> <br />
+        Die Widerrufsfrist beträgt vierzehn Tage, ab dem Tag, an dem Sie oder
+        ein von Ihnen benannter Dritter, der nicht der Beförderer ist, die Waren
+        in Besitz genommen haben bzw. hat.
+        <br /> <br />
+        Um Ihr Widerrufsrecht auszuüben, müssen Sie uns
+        <br />
+        Lakritzel
+        <br />
+        Falk Kreimeier
+        <br />
+        Gesundheitstr. 11
+        <br />
+        D-42699 Solingen
+        <br />
+        E-Mail falk.kreimeier@lakritzel.com
+        <br />
+        mittels einer eindeutigen Erklärung (z.B. ein mit der Post versandter
+        Brief, Telefax oder E-Mail) über Ihren Entschluss, diesen Vertrag zu
+        widerrufen, informieren. Sie können dafür das beigefügte
+        Muster-Widerrufsformular verwenden, das jedoch nicht vorgeschrieben ist.
+        <br /> <br />
+        Widerrufsfolgen
+        <br /> <br />
+        Wenn Sie diesen Vertrag widerrufen, haben wir Ihnen alle Zahlungen, die
+        wir von Ihnen erhalten haben, einschließlich der Lieferkosten (mit
+        Ausnahme der zusätzlichen Kosten, die sich daraus ergeben, dass Sie eine
+        andere Art der Lieferung als die von uns angebotene, günstigste
+        Standardlieferung gewählt haben), unverzüglich und spätestens binnen
+        vierzehn Tagen ab dem Tag zurückzuzahlen, an dem die Mitteilung über
+        Ihren Widerruf dieses Vertrags bei uns eingegangen ist. Für diese
+        Rückzahlung verwenden wir dasselbe Zahlungsmittel, das Sie bei der
+        ursprünglichen Transaktion eingesetzt haben, es sei denn, mit Ihnen
+        wurde ausdrücklich etwas anderes vereinbart; in keinem Fall werden Ihnen
+        wegen dieser Rückzahlung Entgelte berechnet.
+        <br /> <br />
+        Wir können die Rückzahlung verweigern, bis wir die Waren wieder
+        zurückerhalten haben oder bis Sie den Nachweis erbracht haben, dass Sie
+        die Waren zurückgesandt haben, je nachdem, welches der frühere Zeitpunkt
+        ist.
+        <br /> <br />
+        Sie haben die Waren unverzüglich und in jedem Fall spätestens binnen
+        vierzehn Tagen ab dem Tag, an dem Sie uns über den Widerruf dieses
+        Vertrages unterrichten, an uns zurückzusenden oder zu übergeben. Die
+        Frist ist gewahrt, wenn Sie die Waren vor Ablauf der Frist von vierzehn
+        Tagen absenden.
+        <br /> <br />
+        Sie tragen die unmittelbaren Kosten der Rücksendung der Waren.
+        <br /> <br />
+        Ende der Widerrufsbelehrung
+        <br /> <br />
+        ****************************************************************************************************
+        §7 Widerrufsformular
+        <br />
+        Muster-Widerrufsformular
+        <br />
+        (Wenn Sie den Vertrag widerrufen wollen, dann füllen Sie bitte dieses
+        Formular aus und senden Sie es zurück.)
+        <br />
+        An :<br />
+        Lakritzel
+        <br />
+        Falk Kreimeier
+        <br />
+        Gesundheitstr. 11
+        <br />
+        D-42699 Solingen
+        <br />
+        E-Mail falk.kreimeier@lakritzel.com
+        <br />
+        <br /> <br />
+        Hiermit widerrufe(n) ich/wir (*) den von mir/uns (*) abgeschlossenen
+        Vertrag über den Kauf der folgenden Waren (*)/die Erbringung der
+        folgenden Dienstleistung (*)
+        <br /> <br />
+        _____________________________________________________ Bestellt am
+        (*)/erhalten am (*)
+        <br /> <br />
+        __________________
+        <br />
+        Name des/der Verbraucher(s)
+        <br />
+        <br /> <br />
+        _____________________________________________________ Anschrift des/der
+        Verbraucher(s)
+        <br />
+        _____________________________________________________ Unterschrift
+        des/der Verbraucher(s) (nur bei Mitteilung auf Papier)
+        <br />
+        <br /> <br />
+        __________________
+        <br />
+        <br />
+        Datum
+        <br /> <br />
+        __________________
+        <br />
+        (*) Unzutreffendes streichen.
+        <br /> <br />
+        §8 Gewährleistung
+        <br />
+        Es gelten die gesetzlichen Gewährleistungsregelungen.
+        <br /> <br />
+        §9 Vertragssprache
+        <br />
+        Als Vertragssprache steht ausschließlich Deutsch zur Verfügung.
+        <br /> <br />
+        §36, 37 Verbraucherstreitbeilegungsgesetz
+        <br />
+        Die Europäische Kommission stellt eine Plattform für die
+        außergerichtliche Online-Streitbeilegung (OS-Plattform) bereit, die
+        unter www.ec.europa.eu/consumers/odr aufrufbar ist. Unsere
+        E-Mail-Adresse finden Sie in unserem Impressum. Wir sind weder
+        verpflichtet noch bereit, an dem Streitschlichtungsverfahren
+        teilzunehmen.
+        <br /> <br />
+        Kontakt
+        <br /> <br />
+        Falk Kreimeier
+        <br /> <br />
+        Gesundheitstraße 11
+        <br /> <br />
+        42699 Solingen
+        <br /> <br />
+        E-Mail: info@lakritzel.com
+        <br /> <br />
+        © Copyright – Falk Kreimeier – Impressum
+        <br />
+        WP DSGVO Tools (GDPR) for Wordpress and WooCommerce Wir verwenden
+        Cookies, um Ihnen das beste Nutzererlebnis bieten zu können. Wenn Sie
+        fortfahren, diese Seite zu verwenden, nehmen wir an, dass Sie damit
+        einverstanden sind. OK Ablehnen
+      </Text>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  background-color: white;
+`
+
+export default AGB
