ccf16e526a06adc4aa2fc992901fe8c4e34b702c
Falk Kreimeier <f.kreimeier@gmx.net>
2021-11-12 12:47:38 +0100

    delete unnecessary code

--- src/components/Lakritzel/Desktop/DesktopSide.js
+++ src/components/Lakritzel/Desktop/DesktopSide.js
@@ -14,21 +14,20 @@ function DesktopSide({ onAddOrder, data }) {
       <CampaignCardLink />
       <CocktailsLink />
       <ProfileDesktop />
-      <FooterDesktop />
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
   @media only screen and (min-width: 1000px) {
-    width: 100vh;
+    width: 1060px;
+    margin: 0 auto;
     display: grid;
-    grid-gap: 2px;
+    grid-gap: 3px;
+    grid-auto-columns: 1fr 1fr;
     grid-template-areas:
-      'header header header'
       'campaignLink shop kritzelkopf'
-      'productCard shop cocktailsLink'
-      'footerDesktop footerDesktop footerDesktop';
+      'productCard shop cocktailsLink';
   }
 `
 
