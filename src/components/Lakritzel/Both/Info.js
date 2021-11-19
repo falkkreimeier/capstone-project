38e14c6d2702e2495dbde296770a8dbd13da6455
Falk Kreimeier <f.kreimeier@gmx.net>
2021-11-19 13:03:41 +0100

    change height

--- src/components/Lakritzel/Both/Info.js
+++ src/components/Lakritzel/Both/Info.js
@@ -50,7 +50,7 @@ const Wrapper = styled.section`
   align-items: center;
   background-color: white;
   margin: 0 auto;
-  height: 485px;
+  height: 495px;
   width: var(--main-width);
   border-radius: var(--border-radius);
   @media (min-width: 1000px) {
