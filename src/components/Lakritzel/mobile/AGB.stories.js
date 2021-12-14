8700c0942bd6778135ac243dd55a44ce495fb22b
Falk Kreimeier <f.kreimeier@gmx.net>
2021-12-14 16:33:25 +0100

    try to solve a problem with storybook

---
+++ src/components/Lakritzel/mobile/AGB.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AGB from './AGB'
+
+export default {
+  title: 'component/AGB',
+  component: AGB,
+}
+
+const Template = args => <AGB />
+
+export const Standard = Template.bind({})
