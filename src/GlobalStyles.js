21cb59eac53559b5fb50860a61668d34e99bd597
Falk Kreimeier <f.kreimeier@gmx.net>
2022-03-15 11:31:10 +0100

    download google fonts

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -18,6 +18,36 @@ const GlobalStlyles = createGlobalStyle`
     src: url(${Ventana}) format("openType");
 }
 
+
+/* fira-sans-200 - latin */
+@font-face {
+  font-family: 'Fira Sans';
+  font-style: normal;
+  font-weight: 200;
+  src: url('../fonts/fira-sans-v15-latin-200.eot'); /* IE9 Compat Modes */
+  src: local(''),
+       url('../fonts/fira-sans-v15-latin-200.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+       url('../fonts/fira-sans-v15-latin-200.woff2') format('woff2'), /* Super Modern Browsers */
+       url('../fonts/fira-sans-v15-latin-200.woff') format('woff'), /* Modern Browsers */
+       url('../fonts/fira-sans-v15-latin-200.ttf') format('truetype'), /* Safari, Android, iOS */
+       url('../fonts/fira-sans-v15-latin-200.svg#FiraSans') format('svg'); /* Legacy iOS */
+}
+
+/* pragati-narrow-700 - latin */
+@font-face {
+  font-family: 'Pragati Narrow';
+  font-style: normal;
+  font-weight: 700;
+  src: url('../fonts/pragati-narrow-v11-latin-700.eot'); /* IE9 Compat Modes */
+  src: local(''),
+       url('../fonts/pragati-narrow-v11-latin-700.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+       url('../fonts/pragati-narrow-v11-latin-700.woff2') format('woff2'), /* Super Modern Browsers */
+       url('../fonts/pragati-narrow-v11-latin-700.woff') format('woff'), /* Modern Browsers */
+       url('../fonts/pragati-narrow-v11-latin-700.ttf') format('truetype'), /* Safari, Android, iOS */
+       url('../fonts/pragati-narrow-v11-latin-700.svg#PragatiNarrow') format('svg'); /* Legacy iOS */
+}
+
+
 html {
     box-sizing: border-box;
 }
